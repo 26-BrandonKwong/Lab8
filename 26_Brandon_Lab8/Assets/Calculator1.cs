297a56ceb6f206d8c8a466056c6b350343e88ad5
26-BrandonKwong <brandonkwongjunchen@gmail.com>
2024-01-22 12:22:19 +0800

    updated

---
+++ 26_Brandon_Lab8/Assets/Calculator1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator1 : MonoBehaviour
+{
+public InputField txtAmount;
+float SGDAmount;
+public Toggle USD;
+public Toggle JPY;
+float value;
+public Text Convertedvalue;
+float valueamount;
+
+
+public void conversion()
+{
+SGDAmount = float.Parse(txtAmount.text);
+
+if (USD.GetComponent<Toggle>().isOn == true)
+{
+       value = SGDAmount * 0.74f;
+Convertedvalue.text = value.ToString() + " USD";
+
+
+}
+
+
+if(JPY.GetComponent<Toggle>().isOn == true)
+{
+      value = SGDAmount * 110.5f;
+Convertedvalue.text = value.ToString() + " JPY";
+}
+
+}
+}
+
+
+
+
+
