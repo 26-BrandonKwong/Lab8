cf2d12aaa0d35310ef2c22369574e52ad3b4896d
26-BrandonKwong <brandonkwongjunchen@gmail.com>
2024-01-22 10:04:46 +0800

    unity project

---
+++ 26_Brandon_Lab8/Assets/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+public Text displayText;
+
+    private string currentInput = "";
+
+    public void OnButtonClick(string value)
+    {
+        // Append the clicked button value to the input
+        currentInput += value;
+
+        // Update the display
+        displayText.text = currentInput;
+    }
+
+    public void OnClearClick()
+    {
+        // Clear the input
+        currentInput = "";
+
+        // Update the display
+        displayText.text = "0";
+    }
+
+    public void OnCalculateClick()
+    {
+        // Perform currency conversion using the currentInput value
+        // You can call the currency conversion logic here
+
+        // Display the result
+        // displayText.text = result.ToString("F2");
+    }
+}
