a48c4905d8620aebb0f59a0ffa76423d71c56023
AhhhhYe <hiddendreams3112@gmail.com>
2024-01-23 09:28:34 +0800

    Added New Values
    Malaysian Ringgit
    Euro
    Korean Won
    Taiwan Dollar

--- 26_Brandon_Lab8/Assets/Calculator1.cs
+++ 26_Brandon_Lab8/Assets/Calculator1.cs
@@ -9,6 +9,10 @@ public InputField txtAmount;
 float SGDAmount;
 public Toggle USD;
 public Toggle JPY;
+public Toggle Malaysian;
+public Toggle Euro;
+public Toggle korean;
+public Toggle taiwan;
 float value;
 public Text Convertedvalue;
 float valueamount;
@@ -32,8 +36,27 @@ if(JPY.GetComponent<Toggle>().isOn == true)
       value = SGDAmount * 110.5f;
 Convertedvalue.text = value.ToString() + " JPY";
 }
-
-}
+        if (Malaysian.GetComponent<Toggle>().isOn == true)
+        {
+            value = SGDAmount * 3.08f;
+            Convertedvalue.text = value.ToString() + " RM";
+        }
+        if (Euro.GetComponent<Toggle>().isOn == true)
+        {
+            value = SGDAmount * 0.63f;
+            Convertedvalue.text = value.ToString() + " EUR";
+        }
+        if (korean.GetComponent<Toggle>().isOn == true)
+        {
+            value = SGDAmount * 881.54f;
+            Convertedvalue.text = value.ToString() + " KRW";
+        }
+        if (taiwan.GetComponent<Toggle>().isOn == true)
+        {
+            value = SGDAmount * 20.73f;
+            Convertedvalue.text = value.ToString() + " TWD";
+        }
+    }
 }
 
 
